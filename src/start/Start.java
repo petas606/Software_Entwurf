8317ca14a38d471ddf8e2e017a2cfb2225a85e87
Manh Tien Truong <petas606@gmail.com>
2021-12-25 12:04:43 +0100

    fix: added missing start

---
+++ src/start/Start.java
@@ -0,0 +1,12 @@
+package start;
+
+// import dialog.menü.Hauptmenue;
+// import dlg_javafx.Hauptmenue;
+
+public class Start {
+
+		public static void main(String[] args) {
+		
+		// Hauptmenue.Start_Dialog();
+	}
+}
