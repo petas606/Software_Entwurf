a17fa132c257af53f9f3b8fe5d3566378960d3d4
Xuan Bach Le <45541138+bach3142ooo1@users.noreply.github.com>
2021-12-25 13:30:04 +0100

    Merge branch 'Prüfungsleistung-Teil-3' of https://github.com/petas606/Software_Entwurf into Prüfungsleistung-Teil-3
    # Conflicts:
    #	.project
    #	src/start/Start.java

--- src/dialog_javafx/Hauptmenue.java
+++ src/dialog_javafx/Hauptmenue.java
@@ -1,27 +1,26 @@
 package dialog_javafx;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-public class Hauptmenue extends Application {
-	private Stage mainStage; 
-	
+public class Hauptmenue extends Application{
+
+	public static void Start_Dialog() {
+		launch();
+	}
+
 	@Override
-	public void start (Stage primaryStage) {
+	public void start(Stage primaryStage) throws Exception {
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
-//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public static void Start_Dialog() {
-		launch();
-	}
-	
+
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,6 @@
 module Prüfungsleistung_Teil_3 {
 	requires javafx.controls;
 	exports dialog_javafx;
-	opens application to javafx.graphics, javafx.fxml;
+	opens start to javafx.graphics, javafx.fxml;
 	
 }
