02f8da32b90c35d0d0616c3dc57fd2a2b0e55382
h2trungvuong@gmail.com <h2trungvuong@gmail.com>
2021-12-25 13:22:44 +0100

    fix: fix start class

--- src/dialog_javafx/Hauptmenue.java
+++ src/dialog_javafx/Hauptmenue.java
@@ -1,5 +1,27 @@
 package dialog_javafx;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class Hauptmenue {
-
+public class Hauptmenue extends Application {
+	private Stage mainStage; 
+	
+	@Override
+	public void start (Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void Start_Dialog() {
+		launch();
+	}
+	
 }
--- src/start/Start.java
+++ src/start/Start.java
@@ -1,12 +1,12 @@
 package start;
 
+import dialog_javafx.Hauptmenue;
+
 // import dialog.menü.Hauptmenue;
 // import dlg_javafx.Hauptmenue;
 
-public class Start {
-
-		public static void main(String[] args) {
-		
-		// Hauptmenue.Start_Dialog();
-	}
+public class Start {	
+	public static void main(String[] args) {	
+		Hauptmenue.Start_Dialog();
+	}		
 }
